d66437fe8a8f728e3f162c0807bbd44eae277e30
perryhg <perryhg@gmail.com>
2014-05-23 15:03:34 +0800

    move logger function to startudp.sh

--- startudp.sh
+++ startudp.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+PATH=/usr/local/bin:/usr/local/sbin:/usr/bin:/usr/sbin:/bin:/sbin
 cd /backup/app/udpq
-nohup ./udpq.sh &
+nohup ./udpq.sh|logger -t udpq 2>&1 &
 
--- udpq.sh
+++ udpq.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-groovy udpq.groovy|logger -t udpq
+export GROOVY_HOME=/opt/groovy-home
+export JAVA_HOME=/opt/java-home
+export PATH=$GROOVY_HOME/bin:$JAVA_HOME/bin:/usr/local/bin:/usr/local/sbin:/usr/bin:/usr/sbin:/bin:/sbin
+groovy udpq.groovy
